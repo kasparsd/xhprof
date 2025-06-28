62aaf931b19101e51cccce41864f941c65eea029
Kaspars Dambis <hi@kaspars.net>
2025-06-28 16:36:24 +0300

    Add NULL check for ignored functions entry creation in PHP 8.0+
    Fixes segmentation fault when the first function call in a request is an
    ignored function. Previously, the code assumed *(entries) was always
    non-NULL when creating entries for ignored functions, but this caused a NULL pointer dereference when profiling started with an ignored function.
    
    The fix adds a NULL check and uses the actual function name when entries is NULL, ensuring proper initialization of the entry stack even when the first call is ignored.

--- extension/trace.h
+++ extension/trace.h
@@ -158,7 +158,13 @@ static zend_always_inline int begin_profiling(zend_string *root_symbol, zend_exe
     } else {
 #if PHP_VERSION_ID >= 80000
         hp_entry_t *cur_entry = hp_fast_alloc_hprof_entry();
-        (cur_entry)->name_hprof = zend_string_copy((*(entries))->name_hprof);
+        /* Check if entries is not NULL before dereferencing */
+        if (*(entries) != NULL) {
+            (cur_entry)->name_hprof = zend_string_copy((*(entries))->name_hprof);
+        } else {
+            /* If this is the first call and it's ignored, use the function name */
+            (cur_entry)->name_hprof = zend_string_copy(function_name);
+        }
         (cur_entry)->prev_hprof = (*(entries));
         (cur_entry)->is_trace = 0;
         (*(entries)) = (cur_entry);
